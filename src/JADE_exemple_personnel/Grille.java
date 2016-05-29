7043d1f623c77552fa18b53de885650f75ac3fcb
francoisDEAVEIRO <kalupso@live.fr>
2016-05-29 19:19:45 +0200

    grille

--- src/JADE_exemple_personnel/Grille.java
+++ src/JADE_exemple_personnel/Grille.java
@@ -1,9 +1,9 @@
 package JADE_exemple_personnel;
 
 public class Grille {
-    private int tab[][];
-    private int largeur;
-    private int hauteur;
+    public int tab[][];
+    public int largeur;
+    public int hauteur;
     
     public Grille(int l, int h){
         this.largeur = l;
@@ -14,8 +14,12 @@ public class Grille {
     public void init(){
         for(int i=0; i<largeur; i++){
             for(int j=0; j<hauteur; j++){
-                this.tab[i][j] = (int) (Math.random()*100 +50);
+                this.tab[i][j] = 1000;//(int) (Math.random()*100 +50);
             }
         }
+    }
+    
+    public void add (int x, int y, int value){
+        this.tab[x][y] = value;
     }
 }
