2f30260cd92bb4b00b52e941501e051eab09ace2
Victor <victor_giroud@yahoo.fr>
2016-05-30 11:14:16 +0200

    ia agent capteurs

--- src/JADE_exemple_personnel/CapteurAgent.java
+++ src/JADE_exemple_personnel/CapteurAgent.java
@@ -9,6 +9,7 @@ import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.wrapper.StaleProxyException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +18,7 @@ import java.util.logging.Logger;
 public class CapteurAgent extends Agent {
     
     int posX, posY;
+    ArrayList<Coord> historique;
     
     @Override
     protected void setup() 
@@ -38,6 +40,7 @@ public class CapteurAgent extends Agent {
             e.printStackTrace();
         } 
         Random random = new Random();
+        historique = new ArrayList<>();
         
         // Définition de son comportement
         addBehaviour(
@@ -63,34 +66,91 @@ public class CapteurAgent extends Agent {
                     msg.setContent(posX + "," + posY);
                     msg.addReceiver(new AID("MainAgent", AID.ISLOCALNAME));
                     send(msg);
+                    historique.add(new Coord(posX, posY));
                     block(100);
                     
                     // Modification de sa position
+                    
+                    // Si possible l'agent se déplace sur une case qu'il n'a jamais parcouru
                     int rand = random.nextInt(4);
-                    switch (rand) {
-                        case 0:
-                            if(posX>0){
-                                posX--;
+                    int nbEssais = 0;
+                    boolean deplacement = false;
+                    while(nbEssais < 4 && !deplacement){
+                        rand = (rand +1)%4;
+                        switch (rand) {
+                            case 0:
+                                if(posX>0 && !dejaParcouru(posX-1, posY)){
+                                    posX--; 
+                                    deplacement = true;
+                                }
+                                nbEssais++;
                                 break;
-                            }
-                        case 1:
-                            if(posX<99){
-                                posX++;
+                            case 1:
+                                if(posX<99 && !dejaParcouru(posX+1, posY)){
+                                    posX++;
+                                    deplacement = true;
+                                }
+                                nbEssais++;
+                                break;    
+                            case 2:
+                                if(posY>0 && !dejaParcouru(posX, posY-1)){
+                                    posY--;
+                                    deplacement = true;
+                                }
+                                nbEssais++;
                                 break;
-                            }
-                        case 2:
-                            if(posY>0){
-                                posY--;
-                                break;
-                            }
-                        case 3:
-                            if(posY<49){
-                                posY++;
-                                break;
-                            }
-                    }    
+                            case 3:
+                                if(posY<49 && !dejaParcouru(posX, posY+1)){
+                                    posY++;
+                                    deplacement = true;
+                                }
+                                nbEssais++;
+                                break;  
+                        }  
+                    }
+                    
+                    // Déplacement completement aléatoire sinon
+                    if(!deplacement){
+                        rand = random.nextInt(4);
+                        switch (rand) {
+                            case 0:
+                                if(posX>0){
+                                    posX--;
+                                    break;
+                                }
+                            case 1:
+                                if(posX<99){
+                                    posX++;
+                                    break;
+                                }
+                            case 2:
+                                if(posY>0){
+                                    posY--;
+                                    break;
+                                }
+                            case 3:
+                                if(posY<49){
+                                    posY++;
+                                    break;
+                                }
+                        }    
+                    }
                 }
             }                  
         );
     } 
+    
+    
+    private boolean dejaParcouru(int x, int y){
+        return historique.stream().anyMatch((coord) -> (coord.x == x && coord.y == y));
+    }
+    
+    class Coord{
+        public int x, y;
+        
+        public Coord(int _x, int _y){
+            x = _x;
+            y = _y;
+        }
+    }
 }
